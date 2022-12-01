01119a396103b99ad3d8e914c52dadecdb52b198
Sengimu <119593267+Sengimu@users.noreply.github.com>
2022-12-01 20:16:21 +0800

    Delete code directory

--- code/src/main/java/com/work/competition/mapper/TbUserInfoMapper.java
+++
@@ -1,8 +0,0 @@
-package com.work.competition.mapper;
-
-
-import com.work.competition.pojo.TbUserInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface TbUserInfoMapper extends Mapper<TbUserInfo> {
-}
